b36937fde1bf72cd8971d3f87c781ef0d34a7ba4
Blacksplay <Blacksplay.online@gmail.com>
2023-06-19 07:31:15 +0200

    Housing

---
+++ MTK 3/Assets/Scripte/Teller.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teller : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] teller;
+
+    void Update()
+    {
+
+
+        /*if (Input.GetKeyUp("3"))
+        {
+            for (int i = 0; i < teller.Length; i++)
+            {
+                teller[i].SetActive(true);
+                print("Teller an");
+            }
+
+        }
+
+        if (Input.GetKeyUp("4"))
+        {
+            for (int i = 0; i < teller.Length; i++)
+            {
+                teller[i].SetActive(false);
+                print("Teller aus");
+            }
+
+        }*/
+
+
+
+    }
+}
